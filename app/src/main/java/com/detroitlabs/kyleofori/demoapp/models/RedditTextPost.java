c34eb62b8d64bf806cec973704dcb3bc3314531f
Brandy Foster <brandy.foster@detroitlabs.com>
2014-11-17 15:06:09 -0500

    created reddit post model

---
+++ app/src/main/java/com/detroitlabs/kyleofori/demoapp/models/RedditTextPost.java
@@ -0,0 +1,12 @@
+package com.detroitlabs.kyleofori.demoapp.models;
+
+/**
+ * Created by BFineRocks on 11/17/14.
+ */
+public class RedditTextPost {
+    private String title;
+    private String author;
+    private String text;
+    private String url;
+
+}
