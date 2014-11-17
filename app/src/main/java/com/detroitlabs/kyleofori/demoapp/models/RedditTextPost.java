51f61ed846340415c8e85695e73d359fa730d218
Brandy Foster <brandy.foster@detroitlabs.com>
2014-11-17 15:08:45 -0500

    complete post model

--- app/src/main/java/com/detroitlabs/kyleofori/demoapp/models/RedditTextPost.java
+++ app/src/main/java/com/detroitlabs/kyleofori/demoapp/models/RedditTextPost.java
@@ -9,4 +9,26 @@ public class RedditTextPost {
     private String text;
     private String url;
 
+    public RedditTextPost(String title, String author, String text, String url){
+        this.title = title;
+        this.author = author;
+        this.text = text;
+        this.url = url;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public String getUrl() {
+        return url;
+    }
 }
