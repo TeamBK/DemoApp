72910a3fbea2f86e0c8dc4028331123878ca2e35
Kyle Ofori <kyle.ofori@detroitlabs.com>
2014-11-18 15:36:52 -0500

    Making classes for parser and asynctask

---
+++ app/src/main/java/com/detroitlabs/kyleofori/demoapp/parsers/RedditJSONParser.java
@@ -0,0 +1,7 @@
+package com.detroitlabs.kyleofori.demoapp.parsers;
+
+/**
+ * Created by kyleofori on 11/18/14.
+ */
+public class RedditJSONParser {
+}
---
+++ app/src/main/java/com/detroitlabs/kyleofori/demoapp/tasks/GetRedditPostTask.java
@@ -0,0 +1,15 @@
+package com.detroitlabs.kyleofori.demoapp.tasks;
+
+import android.os.AsyncTask;
+
+import java.util.ArrayList;
+
+/**
+ * Created by kyleofori on 11/18/14.
+ */
+public class GetRedditPostTask extends AsyncTask<String, Void, ArrayList<String>> {
+    @Override
+    protected String[] doInBackground(String... strings) {
+        return new String[0];
+    }
+}
