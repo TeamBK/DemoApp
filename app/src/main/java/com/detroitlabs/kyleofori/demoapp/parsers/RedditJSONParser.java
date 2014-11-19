9d1a98499a66cfe2764fc3699d8c613a861f1b58
Brandy Foster <brandy.foster@detroitlabs.com>
2014-11-18 21:27:10 -0500

    added parsing final strings

--- app/src/main/java/com/detroitlabs/kyleofori/demoapp/parsers/RedditJSONParser.java
+++ app/src/main/java/com/detroitlabs/kyleofori/demoapp/parsers/RedditJSONParser.java
@@ -1,15 +1,29 @@
 package com.detroitlabs.kyleofori.demoapp.parsers;
 
+import com.detroitlabs.kyleofori.demoapp.models.RedditTextPost;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 /**
  * Created by kyleofori on 11/18/14.
  */
 public class RedditJSONParser {
+    RedditTextPost redditTextPost;
 
-    public String convertJSONStringToString(String JSONString){
+    public String convertJSONStringToRedditObject(String JSONString) throws JSONException{
+        final String redditDataObject = "data";
+        final String redditChildrenArray = "children";
         final String redditPostAuthor = "author";
         final String redditPostTitle = "title";
-        final String redditPostText = "text";
+        final String redditPostText = "selftext";
         final String redditPostUrl = "url";
+
+        JSONObject jsonObject = new JSONObject(JSONString);
+        JSONObject jsonDataObject = jsonObject.getJSONObject(redditDataObject);
+        JSONArray jsonChildrenArray = jsonDataObject.getJSONArray(redditChildrenArray);
+
     }
 
 }
