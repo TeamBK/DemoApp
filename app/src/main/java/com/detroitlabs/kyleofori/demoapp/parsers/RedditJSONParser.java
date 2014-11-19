26228a0e38a28fcf3eec1f7d341ac9127b3c254f
Brandy Foster <brandy.foster@detroitlabs.com>
2014-11-18 21:15:58 -0500

    created method stub and variables for converting json to string

--- app/src/main/java/com/detroitlabs/kyleofori/demoapp/parsers/RedditJSONParser.java
+++ app/src/main/java/com/detroitlabs/kyleofori/demoapp/parsers/RedditJSONParser.java
@@ -5,4 +5,11 @@ package com.detroitlabs.kyleofori.demoapp.parsers;
  */
 public class RedditJSONParser {
 
+    public String convertJSONStringToString(String JSONString){
+        final String redditPostAuthor = "author";
+        final String redditPostTitle = "title";
+        final String redditPostText = "text";
+        final String redditPostUrl = "url";
+    }
+
 }
