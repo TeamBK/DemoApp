fc90a836109084d98fed2127a5f8cbb76bd09428
Kyle Ofori <kyle.ofori@detroitlabs.com>
2014-11-20 22:17:22 -0500

    Getting rid of commented-out information in GetRedditPostTask

--- app/src/main/java/com/detroitlabs/kyleofori/demoapp/tasks/GetRedditPostTask.java
+++ app/src/main/java/com/detroitlabs/kyleofori/demoapp/tasks/GetRedditPostTask.java
@@ -3,6 +3,7 @@ package com.detroitlabs.kyleofori.demoapp.tasks;
 import android.net.Uri;
 import android.util.Log;
 
+import com.detroitlabs.kyleofori.demoapp.models.RedditTextPost;
 import com.detroitlabs.kyleofori.demoapp.parsers.RedditJSONParser;
 
 import org.json.JSONException;
@@ -20,6 +21,7 @@ import java.net.URL;
  */
 public class GetRedditPostTask implements Runnable /*extends AsyncTask<String, Void, ArrayList<RedditTextPost>> */{
     private static final String LOG_TAG = GetRedditPostTask.class.getSimpleName();
+    RedditTextPost redditTextPost;
     RedditJSONParser redditJSONParser = new RedditJSONParser();
     String jsonText = null;
     String searchTerm;
@@ -27,78 +29,6 @@ public class GetRedditPostTask implements Runnable /*extends AsyncTask<String, V
     public GetRedditPostTask(String searchTerm){
      this.searchTerm = searchTerm;
     }
-
-/*    @Override
-    protected ArrayList<RedditTextPost> doInBackground(String... strings) {
-        Uri.Builder builder = new Uri.Builder();
-        builder.scheme("http")
-                .authority("api.reddit.com")
-                .appendPath("r")
-                .appendPath(strings[0]);
-        String urlString = builder.build().toString();
-
-        HttpURLConnection connection = null;
-        URL url;
-        String line;
-        BufferedReader bufferedReader = null;
-        try {
-            url = new URL(urlString);
-            connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("GET");
-            connection.connect();
-
-            InputStream inputStream = connection.getInputStream();
-            StringBuffer stringBuffer = new StringBuffer();
-            if(inputStream == null) {
-                return null;
-            }
-
-            bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-
-            while ((line = bufferedReader.readLine())!=null) {
-                stringBuffer.append(line+"\n");
-            }
-
-            if(stringBuffer.length()==0) {
-                return null;
-            }
-
-            jsonText = stringBuffer.toString();
-            Log.d(LOG_TAG, jsonText);
-        }
-        catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        finally {
-            if(connection!=null) {
-                connection.disconnect();
-            }
-            if(bufferedReader!=null) {
-                try{
-                    bufferedReader.close();
-                } catch (final IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-        try{
-            redditJSONParser.convertJSONStringToRedditObject(jsonText);
-            return redditJSONParser.getRedditTextPostArrayList();
-
-        }catch(JSONException e){
-            e.printStackTrace();
-        }
-
-        return null;
-    }
-
-    @Override
-    protected void onPostExecute(ArrayList<RedditTextPost> redditPostArrayList) {
-        super.onPostExecute(redditPostArrayList);
-    }*/
 
     @Override
     public void run() {
@@ -157,7 +87,7 @@ public class GetRedditPostTask implements Runnable /*extends AsyncTask<String, V
             }
         }
         try{
-            redditJSONParser.convertJSONStringToRedditObject(jsonText);
+            redditTextPost = redditJSONParser.convertJSONStringToRedditObject(jsonText);
 
 
         }catch(JSONException e){
