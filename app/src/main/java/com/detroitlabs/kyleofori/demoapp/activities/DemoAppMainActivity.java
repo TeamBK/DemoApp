14c7a2dcd5f1e1b71388c6283967de2f3debd935
Ken Luk <ken.luk@detroitlabs.com>
2014-11-21 11:05:44 -0500

    refactored main activity

--- app/src/main/java/com/detroitlabs/kyleofori/demoapp/activities/DemoAppMainActivity.java
+++ app/src/main/java/com/detroitlabs/kyleofori/demoapp/activities/DemoAppMainActivity.java
@@ -15,11 +15,9 @@ import com.detroitlabs.kyleofori.demoapp.backgroundthreadscheduler.RepeatingPost
 
 
 public class DemoAppMainActivity extends Activity {
-    private static final String LOG_TAG = DemoAppMainActivity.class.getSimpleName();
     public static final String SUBREDDIT_NAME = "subredditname";
     private EditText edtSubredditName;
     private Button btnSubmit;
-    RepeatingPostFetchExecutor repeatingTask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,34 +28,13 @@ public class DemoAppMainActivity extends Activity {
         btnSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Log.d(LOG_TAG, edtSubredditName.getText().toString());
                 btnSubmit.setEnabled(false);
                 String subredditName = edtSubredditName.getText().toString();
                 Intent intent = new Intent(DemoAppMainActivity.this, SubRedditActivity.class);
                 intent.putExtra(SUBREDDIT_NAME, subredditName);
                 startActivity(intent);
-
             }
         });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.demo_app, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 
     @Override
