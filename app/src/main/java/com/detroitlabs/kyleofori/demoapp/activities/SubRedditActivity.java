22b7ae0261959e88dd07347dc25467b5b0727f69
Brandy Foster <brandy.foster@detroitlabs.com>
2014-11-21 10:19:16 -0500

    pulled method out of constructor to start repeater. added method call to onStart and onResume. Cleaned up some commented out stuff

--- app/src/main/java/com/detroitlabs/kyleofori/demoapp/activities/SubRedditActivity.java
+++ app/src/main/java/com/detroitlabs/kyleofori/demoapp/activities/SubRedditActivity.java
@@ -1,13 +1,9 @@
 package com.detroitlabs.kyleofori.demoapp.activities;
 
 import android.app.Activity;
-import android.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
 
 import com.detroitlabs.kyleofori.demoapp.R;
 import com.detroitlabs.kyleofori.demoapp.fragments.RedditListFragment;
@@ -45,19 +41,4 @@ public class SubRedditActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_reddit_list, container, false);
-            return rootView;
-        }
-    }
 }
